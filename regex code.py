294ebcd619eec0251271402f5e9a7883d122bf82
Jens Jensen <albecker@hotmail.de>
2019-04-23 00:14:47 +0200

    regex code zinzugegt

---
+++ regex code.py
@@ -0,0 +1,6 @@
+import re
+Url = "https://manganelo.com/manga/read_berserk_manga_online"
+matches = re.search('title=\"([\w\s:\-!;\d]+)Chapter ([\d\.]+)(?:\s?: ([\w\s!]+))?\"', Url)
+comic_title = matches.group(1)
+chapter = matches.group(2)
+chapter_name = matches.group(3)

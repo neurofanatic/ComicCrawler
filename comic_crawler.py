ee71779533af11cdd2520604e37a6ed7a7e1e9a3
Jens Jensen <albecker@hotmail.de>
2019-04-28 16:39:52 +0200

    erster versuch eines codes

---
+++ comic_crawler.py
@@ -0,0 +1,42 @@
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+from urllib.request import Request
+
+
+
+Url = "https://manganelo.com/manga/read_berserk_manga_online"
+
+
+def fetchPage(url):  #input url -----> ouput: page_html
+  req = Request(
+    url,
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    }
+  )
+  #opening connection grabbing the page
+  uClient = uReq(req)
+  page_html = uClient.read()
+  uClient.close()
+
+  return page_html
+
+html = fetchPage(Url)
+
+def findChapterUrls(html):
+  parsed_html  = soup(html, "html.parser")
+  chapter_tags = parsed_html.find_all(class_="row")
+  atag  = []
+  print(chapter_tags[55].find("a")==True)
+  # for rowtag in chapter_tags:
+  #   rowtag.find("a")
+
+  # print(atag)
+
+findChapterUrls(html)
+
+
+
+

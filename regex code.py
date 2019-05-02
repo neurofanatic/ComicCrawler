17fc8dd3d7e3bda76b92b77d95950de3867aed7b
neurofanatic <tgbnhz6@posteo.de>
2019-05-02 20:44:48 +0200

    reference program

--- regex code.py
+++ regex code.py
@@ -1,4 +1,5 @@
 import re
+
 Url = "https://manganelo.com/manga/read_berserk_manga_online"
 matches = re.search('title=\"([\w\s:\-!;\d\.]+)Chapter ([\d\.\s\w]+)(?:\s?: ([\w\s!]+))?\"', Url)
 comic_title = matches.group(1)

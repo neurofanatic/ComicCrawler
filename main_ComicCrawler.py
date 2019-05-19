f1b98f9db2d65d4495cb5ed255bd21d0ce51d2d2
neurofanatic <tgbnhz6@posteo.de>
2019-05-19 19:26:06 +0200

    update_BUILD_1

--- main_ComicCrawler.py
+++ main_ComicCrawler.py
@@ -1,17 +1,17 @@
-#| IMPORTS |-----------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------| IMPORTS |
 import urllib.request 
 from bs4 import BeautifulSoup 
 import re
 import os
-# import regex_code as regex
 
-#| VARIABLES |---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------| VARIABLES |
 URL = 'https://manganelo.com/manga/tales_of_demons_and_gods'
 chapter_urls = []
 image_list = []
 data_list = []
+index = 0
 
-#| DATA COLLECTION |---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------| DATA COLLECTION |
 def getdata_dict (a):
     matches = re.search('title=\"(.+)chapter ([\d\.\s\w]+)(?:\s?: (.+))?\">', a)
     title = matches.group(1)
@@ -19,7 +19,7 @@ def getdata_dict (a):
     chapter_name = matches.group(3)
     return {'title': title, 'chapter': chapter, 'chapter_name': chapter_name}
 
-#| ZUGRIFF WEBSITE URL |-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------| ACCESS WEBSITE URL |
 def html_request(url):
     user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
     headers = {'User-Agent':user_agent,}
@@ -29,24 +29,14 @@ def html_request(url):
     soup = BeautifulSoup(data, 'lxml')
     return soup
 
-#| ZUGRIFF CHAPTER URL |--------------------------------------------------------------------------------
-for item in html_request(URL).select(".row a"):   
-    low = item
-    low = str(low).lower()
-    data_list.append(getdata_dict(low))
-    chapter_urls.append(item.get('href'))
-    
-#| ZUGRIFF IMAGE URL |---------------------------------------------------------------------------------
-for item_a in chapter_urls:
-    for item_b in html_request(item_a).select("#vungdoc img"):
-        image_list.append(item_b.get('src'))
-    data_list[0].update( {'img': image_list})
-  
-#| DOWNLOAD IMAGES |-----------------------------------------------------------------------------------
-def dwnld(url):
-    temp_t = data_list[0]['title']
+# ------------------------------------------------------------------------------| DIRECTORY CREATION |
+def dwnld(data):
+    #for item in length ?!
+    index = 0
+    temp_t = data[index]['title']
+    temp_p =  os.getcwd()
     path_p = 'C:\\ComicCrawler\\{}'
-
+    
     try:
         if not os.path.exists(path_p.format(temp_t)):
             os.makedirs(path_p.format(temp_t)) 
@@ -55,23 +45,38 @@ def dwnld(url):
 
     os.chdir(path_p.format(temp_t))
 
-    for item in url:
-        temp_c = data_list['chapter']
-        temp_n = data_list['chapter_name']
-        temp_p =  os.getcwd()
+    for item in data[index]['img']:
+        temp_c = data[index]['chapter']
+        temp_n = data[index]['chapter_name']
         path_c = temp_p + '{}' + '{}'
 
         try:
             if not os.path.exists(path_c.format('\\Chapter_' + temp_c, '-' + temp_n)):
                 os.mkdir(path_c.format('\\Chapter_' + temp_c, '-' + temp_n)) 
+            else:
+                os.chdir(path_c.format('\\Chapter_' + temp_c, '-' + temp_n))
         except OSError:
             print("Creation of the chapter directory failed")
-            
-    #     filename = item.split('/')[-1]         
-    #     re.search('%s ', item) % s
-    #   urllib.request.urlretrieve(url, path)
 
+        #----------------------------------------------------------------------------| IMAGE REQUEST |
+        filename = item.split('/')[-1] 
+        urllib.request.urlretrieve(item, path_c) !! SERVER_ERROR 403 !!
+        index = index + 1
+
+#-------------------------------------------------------------------------------| ACCESS CHAPTER URL |
+for item in html_request(URL).select(".row a"):   
+    low = item
+    low = str(low).lower()
+    data_list.append(getdata_dict(low))
+    chapter_urls.append(item.get('href'))
+    
+#---------------------------------------------------------------------------------| ACCESS IMAGE URL |
+for item_a in chapter_urls:
+    image_list = []
+    for item_b in html_request(item_a).select("#vungdoc img"):
+        image_list.append(item_b.get('src'))
+    data_list[index].update( {'img': image_list})
+    index = index + 1
+
+#-----------------------------------------------------------------------------------------| DOWNLOAD |
 dwnld(data_list)
-
-
-
